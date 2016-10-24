6923d3b9dc36457f4b4e027fef1c1b17113d5067
Sakaue Aoi <aoiue.w.bbb@gail.com>
2016-10-24 22:04:51 +0900

    no message

--- Clap/ViewController.swift
+++ Clap/ViewController.swift
@@ -10,34 +10,34 @@ import UIKit
 
 import AVFoundation
 
-
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate,UIPickerViewDataSource {
 
     @IBOutlet var clapPickerView: UIPickerView!
     
     var audioPlayer : AVAudioPlayer!
     var soundCount : Int = 0
     
+    
+    var array = [0,1,2,3,4]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        
 //再生する音源のURLを作成
-        let soundFilePath = NSBundle.mainBundle().pathForResource("clap",ofType: "wav")!
+        let soundFilePath = NSBundle.mainBundle().pathForResource("clap", ofType: "wav")!
         
-
+//！入れるとエラーが出る
         let fileURL = NSURL (fileURLWithPath : soundFilePath)
-        
         
         do{
             audioPlayer = try AVAudioPlayer(contentsOfURL: fileURL)
         }catch{
             print("音楽ファイルが読み込めませんでした")
         }
-//                clapPickerView.delegate = self
-//                clapPickerView.dataSource = self
+                clapPickerView.delegate = self
+                clapPickerView.dataSource = self
 
     }
     
@@ -52,13 +52,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 //いくつカテゴリーを持つか
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int{
         return 1
     }
 //いくつ選択肢を作るか
     func pickerView(pickerView: UIPickerView,numberOfRowslnComponent component: Int) ->Int{
-        return 10
+        return array.count
     }
 // 選択肢に何を表示していくか１
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int)->String? {
